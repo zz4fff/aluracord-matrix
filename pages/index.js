2e0014df4b359648651e28a96484db352c71c636
zz4fff <flaviocefetrp@gmail.com>
2022-01-26 01:22:49 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
+import React from 'react';
+
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
+
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
+
 
 // Componente React
 function Titulo(props) {
@@ -62,14 +40,17 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = React.useState('zz4fff');
+  const [valueLength, setValueLength] = React.useState(6);
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
           backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
@@ -93,17 +74,37 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text 
+              variant="body3" 
+              styleSheet={{ 
+                marginBottom: '32px', 
+                color: appConfig.theme.colors.neutrals[300] 
+              }}
+            >
               {appConfig.name}
             </Text>
 
             <TextField
+              value={username}
+              onChange={function (event) {
+                const valor = event.target.value;
+                const tamanho = valor.length;
+
+                setUsername(valor);
+                setValueLength(tamanho);
+              }
+
+              }
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -117,6 +118,8 @@ export default function PaginaInicial() {
             <Button
               type='submit'
               label='Entrar'
+              enabled={valueLength > 2}
+              
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
